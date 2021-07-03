a0cfaaca2a0342f045a4c497b6d3c5324c04637f
Chandar-ai <55431992+Chandar-ai@users.noreply.github.com>
2021-07-04 01:09:24 +0530

    Add files via upload

---
+++ Ngram_App_Code.R
@@ -0,0 +1,139 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+## Shiny App 
+#' This R script will create a Shiny App in which a word or phrase can be given as an input in a text box
+#' and a predicted next word will appear as an output. 
+
+
+library(shiny)
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(stringr)
+})
+
+
+#' Source ngram matching function
+source("./ngram_model_for_word_prediction/Ngram_Word_Prediction_Function_For_ShinyApp.R")
+
+
+
+#' Define UI for application that takes a word or phrase as an input and predicts the next word as an output
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Word Prediction Model"),
+  p("This app will read an input phrase or incomplete sentense that we type in a text box and will show the predicted next word as an output."),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      h2("Instructions:"), 
+      h5("1. Enter one or more than one word in the given text box"),
+      h5("2. Immediately, the next word predicted by the model will appear below in red color"),
+      h5("3. If there is no prediction or misspelling of input words then #No Prediction# will appear"),
+      h5("4. Additional tabs display barplots of the top ngrams (Frequency Distribution of Words) in the dataset"),
+      br(),
+      # a("Source Code", href = "https://github.com/Chandar-ai/ngram_model_for_word_prediction")
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel( 
+      tabsetPanel(
+        tabPanel("Prediction of next word",
+                 textInput("user_input", h3("Type your words in the below box:"), 
+                           value = ""),
+                 h3("Predicted Next Word:"),
+                 h4(em(span(textOutput("ngram_output"), style="color:red")))),
+        
+        tabPanel("top quadgrams",
+                 # br(),
+                 imageOutput("barplot4")),
+        
+        tabPanel("top trigrams",
+                 # br(),
+                 imageOutput("barplot3")),
+        
+        tabPanel("top bigrams",
+                 # br(),
+                 imageOutput("barplot2"))
+        
+      )   
+    )
+  )
+)
+
+
+
+# Define Server logic to predict next word and display barplots
+
+library(shiny)
+library(png)
+
+toptable4 = head(na.omit(readRDS("./ngram_model_for_word_prediction/ngram_app_data/four_words_fast.rds")))
+toptable3 = head(na.omit(readRDS("./ngram_model_for_word_prediction/ngram_app_data/three_words_fast.rds")))
+toptable2 = head(na.omit(readRDS("./ngram_model_for_word_prediction/ngram_app_data/two_words_fast.rds")))
+
+
+#' Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  output$ngram_output <- renderText({
+    ngrams(input$user_input)
+  })
+  
+  output$barplot4 <- renderImage({
+    outfile4 <- tempfile(fileext = '.png')
+    png(outfile4, width = 800, height = 500)
+    table4 = toptable4
+    table4$xvar = paste(toptable4$word1, toptable4$word2, toptable4$word3, toptable4$word4)
+    barplot(table4$n, names=(table4$xvar), col='darkgreen')
+    dev.off()
+    
+    list(src = outfile4)
+    # alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+  
+  output$barplot3 <- renderImage({
+    outfile3 <- tempfile(fileext = '.png')
+    png(outfile3, width = 800, height = 500)
+    table3 = toptable3
+    table3$xvar = paste(toptable3$word1, toptable3$word2, toptable3$word3)
+    barplot(table3$n, names=(table3$xvar), col='brown')
+    dev.off()
+    
+    list(src = outfile3,
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+  
+  output$barplot2 <- renderImage({
+    outfile2 <- tempfile(fileext = '.png')
+    png(outfile2, width = 800, height = 500)
+    table2 = toptable2
+    table2$xvar = paste(toptable2$word1, toptable2$word2)
+    barplot(table2$n, names=(table2$xvar), col='pink')
+    dev.off()
+    
+    list(src = outfile2,
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+}
+
+#' Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
+
---
+++ Ngram_Word_Prediction_Function_For_ShinyApp.R
@@ -0,0 +1,109 @@
+
+
+# Reading Required Libraries 
+
+library(ngram)
+library(NLP)
+library(tm)
+library(corpus)
+library(ggplot2)
+library(stringi)
+library(slam)
+# install.packages("remotes")
+library(remotes)
+remotes::install_github("news-r/textblob")
+
+library(textblob)
+library(RWeka)
+library(LSD) 
+library(dplyr)
+library(stringr)
+
+library(tidytext)
+library(tidyverse)
+library(knitr)
+library(png)
+
+library(dplyr)
+
+
+# Load Training Data, which alread created 
+
+n2_words <- readRDS("./ngram_model_for_word_prediction/ngram_app_data//two_words_fast.rds")
+n3_words  <- readRDS("./ngram_model_for_word_prediction/ngram_app_data//three_words_fast.rds")
+n4_words <- readRDS("./ngram_model_for_word_prediction/ngram_app_data//four_words_fast.rds")
+
+
+# Creation of Ngram Matching Functions
+
+# BIGRAM Matching Function 
+# Finding the frequency distribution of pairs of words, using which, the 2nd word with high frequency is selected. 
+bigram <- function(input_words){
+  n2_words = na.omit(n2_words)
+  num <- length(input_words)
+  w1 = n2_words[n2_words$word1 %in% input_words[num],]
+  top = top_n(w1, 1, n)
+  top1 = filter(top, row_number() == 1L) 
+  next_word = select(top1, num_range("word", 2))
+  out = as.character(next_word)
+  res = ifelse(out =="character(0)", "#No Prediction#", out)
+  return(res)
+}
+
+
+# TRIGRAM Matching Function 
+# Finding the frequency distribution of three set of words (or triplet), using which, the 3rd word with high frequency is selected 
+trigram <- function(input_words){
+  n3_words = na.omit(n3_words)
+  num <- wordcount(input_words)
+  w1 = n3_words[n3_words$word1 %in% word(input_words,num-1),]
+  w2 = w1[w1$word2 %in% word(input_words,num),]
+  top = top_n(w2, 1, n)
+  top1 = filter(top, row_number() == 1L) 
+  next_word = select(top1, num_range("word", 3))
+  out = as.character(next_word)
+  res = ifelse(out =="character(0)", bigram(input_words), out)
+  return(res)
+}
+
+
+# QUADGRAM Matching Function 
+# Finding the frequency distribution of four set of words (or quads), using which, the 4th word with high frequency is selected.
+quadgram <- function(input_words){
+  n4_words = na.omit(n4_words)
+  num <- wordcount(input_words)
+  w1 = n4_words[n4_words$word1 %in% word(input_words,num-2),]
+  w2 = w1[w1$word2 %in% word(input_words,num-1),]
+  w3 = w2[w2$word3 %in% word(input_words,num),]
+  
+  top = top_n(w3, 1, n)
+  top1 = filter(top, row_number() == 1L) 
+  next_word = select(top1, num_range("word", 4))
+  out = as.character(next_word)
+  res = ifelse(out =="character(0)", trigram(input_words), out)
+  return(res)
+}
+
+
+# Creatiion of User Input and Data Cleaning Function
+# This 'ngrams' function will call the matching functions which we have defined above.
+
+ngrams <- function(input){
+  # Creation of a dataframe
+  input <- data_frame(text = input)
+  # Clean the Inpput
+  replace_reg <- "[^[:alpha:][:space:]]*"
+  input <- input %>%
+    mutate(text = str_replace_all(text, replace_reg, ""))
+  # Finding word count, separate words, lower case
+  input_count <- str_count(input, boundary("word"))
+  input_words <- unlist(str_split(input, boundary("word")))
+  input_words <- tolower(input_words)
+  # Calling the matching functions
+  out <- ifelse(input_count == 0, "#No Prediction#",
+                ifelse(input_count == 3, quadgram(input_words),
+                       ifelse(input_count == 2, trigram(input_words), bigram(input_words))))
+  # Output
+  return(out)
+}
+
